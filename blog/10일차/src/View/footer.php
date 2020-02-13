7198b92efd604368e08a9e0c28a5d6ded4f0f6bd
jjine <kamisuk@naver.com>
2020-02-13 21:32:50 +0900

    20200213

---
+++ blog/10일차/src/View/footer.php
@@ -0,0 +1,12 @@
+<div class="quick-links">
+            <ul>
+                <li><a href="https://twitter.com/"><i class="fab fa-twitter"></i></a><p>Twitter</p></li>
+                <li><i class="fab fa-facebook"></i><p>Facebook</p>
+                </li>
+                <li><i class="fab fa-instagram"></i><p>Instagram</p></li>
+                <li><i class="fab fa-github"></i><p>Github</p></li>  
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
